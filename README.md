fbf448767c3464a97bf5faecc769339e0c4a4035
Lucas Pedroso <123400567+LucasFPedrosoDEV@users.noreply.github.com>
2023-06-29 09:37:34 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Formulario-DevQuest
+## Projeto com o intuito de aprimorar o conhecimento em JS
+
+<br>
+
+## Como o projeto funciona ?
+
+Consiste em preencher o formulario, adicionando os dados solicitados, caso o preenchimento for correto os inputs ficaram na cor verde, e se tiver alguma incoerência nos dados ficará com a borda na vermelha.
+
+<br>
+
+## Linguagens usadas :
+- HTML 
+
+- CSS
+
+- JS
+
+<br>
+
+## Versão Desktop :
+
+<br>
+<img src ="./projetoquest1.gif" alt ="versao desktop do projeto quest html css avançado">
+
+<br>
+
+## Versão Mobile :
+<br>
+<img src ="./projetoquest.gif" alt ="versao mobile do projeto quest html css js intermediario">
---
+++ src/js/config.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById("form");
+const camposObrigatorios = document.querySelectorAll("input, textarea");
+
+formulario.addEventListener("submit", function (event) {
+	event.preventDefault();
+	validarCampoVazio(camposObrigatorios);
+});
+
+function validarCampoVazio(camposObrigatorios) {
+	camposObrigatorios.forEach(item => {
+		const controle = item.parentElement;
+		const mensagemErro = controle.querySelector(".obrigatorio");
+		if (item.value === "") {
+			mensagemErro.classList.add("obrigatorio-visivel");
+
+			item.classList.add("campo-vazio");
+			item.classList.remove("campo-preenchido");
+		} else {
+			item.classList.add("campo-preenchido");
+			item.classList.remove("campo-vazio");
+			mensagemErro.classList.remove("obrigatorio-visivel");
+		}
+	});
+}
+function somenteNumeros(e) {
+	var code = e.which || e.keyCode;
+
+	if (code < 48 || code > 57) {
+		e.preventDefault();
+		return false;
+	}
+}
